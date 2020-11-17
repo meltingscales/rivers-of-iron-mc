b24b5435ccf467e1265e217ebc851d1edc998598
Henry Post <henryfbp@gmail.com>
2020-11-17 15:26:12 -0600

    add notes, make sure multimc exists

--- scripts/test-with-pyautogui.py
+++ scripts/test-with-pyautogui.py
@@ -3,24 +3,55 @@ import os
 import psutil
 import distutils.spawn
 import platform
+from pprint import pprint
 
 IS_WINDOWS = platform.system() == 'Windows'
 
-def ensure_packwiz():    
-    if not distutils.spawn.find_executable("packwiz"):
-        raise Exception("`packwiz` not found on PATH. Download from https://github.com/comp500/packwiz")
+LINUX_MMC_PATHS = [
+    '/opt/multimc/run.sh',
+]
 
-def ensure_mmc_closed():        
+WINDOWS_MMC_PATHS = [
+    'C:/tools/MultiMC/MultiMC.exe',
+    'C:/Program Files (x86)/MultiMC/MultiMC.exe'
+    'C:/Program Files/MultiMC/MultiMC.exe'
+]
+
+def get_multimc_path():
+    if IS_WINDOWS:
+        for path in WINDOWS_MMC_PATHS:
+            if os.path.exists(path):
+                return path
+    else:
+        for path in LINUX_MMC_PATHS:
+            if os.path.exists(path):
+                return path
+    return None
+
+def ensure_packwiz_installed():
+    if not distutils.spawn.find_executable("packwiz"):
+        raise Exception(
+            "`packwiz` not found on PATH. Download from https://github.com/comp500/packwiz")
+
+
+def ensure_multimc_installed():
+    if not get_multimc_path():
+        print("Could not find MultiMC instance. Searched these paths:")
+        pprint(LINUX_MMC_PATHS)
+        pprint(WINDOWS_MMC_PATHS)
+        raise Exception("Could not find MMC instance.")
+
+def ensure_multimc_closed():
     # multimc must be closed
-    multimc_open=False
+    multimc_open = False
     for p in psutil.process_iter():
-        
-        name, cmdline="",""
+
+        name, cmdline = "", ""
 
         try:
             name = p.name()
-            cmdline=p.cmdline()
-        except (PermissionError,psutil.AccessDenied) as e: # Windows can do this
+            cmdline = p.cmdline()
+        except (PermissionError, psutil.AccessDenied) as e:  # Windows can do this
             # print("Not allowed to view process {}".format(p.pid))
             pass
 
@@ -28,16 +59,34 @@ def ensure_mmc_closed():
 
         if 'MultiMC' in name or 'MultiMC' in ' '.join(cmdline):
             print(p)
-            multimc_open=True
+            multimc_open = True
 
     if multimc_open:
         raise Exception("MultiMC must be CLOSED for this to work.")
 
-def open_multimc():
-    pass #TODO
 
+def open_multimc():
+    path = get_multimc_path()
+    print("Exec {}".format(path))
+    os.system(path)
 
 if __name__ == '__main__':
-    ensure_packwiz()
+    ensure_packwiz_installed()
+    ensure_multimc_installed()
 
-    ensure_mmc_closed()
+    ensure_multimc_closed()
+    open_multimc()
+
+    '''
+    <ESC>
+    Add instance
+    Import from zip
+    run `packwiz cf export`
+    paste path
+    click on ok
+    wait for import
+    launch
+    wait and check for main menu, wait for crashes
+    terminate mmc
+    exit 0 or 1!
+    '''
