ec22a55168260dfd25f3d03698079f9a6c4e765e
Henry Post <HenryFBP@gmail.com>
2020-11-17 19:53:31 -0600

    attempt to focus window

--- scripts/test-with-pyautogui.py
+++ scripts/test-with-pyautogui.py
@@ -3,21 +3,24 @@ Test the modpack by launching MultiMC, importing and running the pack.
 
 Exits with different error codes depending on what went wrong.
 '''
-
-from os import remove
-import pyautogui
-import time
-import os
-import subprocess
-import psutil
-import distutils.spawn
-import platform
 from pprint import pprint
+import distutils.spawn
+import psutil
+import subprocess
+import os
+import time
+from pygetwindow import BaseWindow
+import pygetwindow as gw
+import pyautogui as pag
+from psutil import Process
+from os import remove
+import platform
 
 IS_WINDOWS = platform.system() == 'Windows'
 
+
 ZIP_NAME = '.PYAUTOGUI-TEST-MODPACK-EXPORT.tmp.zip'
-DEFAULT_ZIP_NAME='export.zip'
+DEFAULT_ZIP_NAME = 'export.zip'
 
 MMC_PATHS = [
     '/opt/multimc/run.sh',
@@ -27,15 +30,40 @@ MMC_PATHS = [
     'C:/Program Files/MultiMC/MultiMC.exe',
 ]
 
+
+def is_multimc(p: Process) -> bool:
+    ismmc = False
+
+    name, cmdline = "", ""
+
+    try:
+        name = p.name()
+        cmdline = p.cmdline()
+    except (PermissionError, psutil.AccessDenied) as e:  # Windows can do this
+        # print("Not allowed to view process {}".format(p.pid))
+        pass
+
+    # print(name, cmdline)
+
+    if 'MultiMC' in name or 'MultiMC' in ' '.join(cmdline):
+        # if 'MultiMC' in name:
+        print(p)
+        ismmc = True
+
+    return ismmc
+
+
 def remove_file(fp):
     if os.path.exists(fp):
         os.remove(fp)
+
 
 def get_multimc_path():
     for path in MMC_PATHS:
         if os.path.exists(path):
             return path
     return None
+
 
 def ensure_packwiz_installed():
     if not distutils.spawn.find_executable("packwiz"):
@@ -49,25 +77,12 @@ def ensure_multimc_installed():
         pprint(MMC_PATHS)
         raise Exception("Could not find MMC instance.")
 
+
 def ensure_multimc_closed():
     # multimc must be closed
     multimc_open = False
     for p in psutil.process_iter():
-
-        name, cmdline = "", ""
-
-        try:
-            name = p.name()
-            cmdline = p.cmdline()
-        except (PermissionError, psutil.AccessDenied) as e:  # Windows can do this
-            # print("Not allowed to view process {}".format(p.pid))
-            pass
-
-        # print(name, cmdline)
-
-        if 'MultiMC' in name or 'MultiMC' in ' '.join(cmdline):
-            print(p)
-            multimc_open = True
+        multimc_open = is_multimc(p)
 
     if multimc_open:
         raise Exception("MultiMC must be CLOSED for this to work.")
@@ -79,30 +94,58 @@ def open_multimc() -> subprocess.Popen:
     # os.system(path) # NOTE: this blocks.
     return subprocess.Popen([path])
 
+
+def get_multimc_window_title() -> str:
+    windowTitles = gw.getAllTitles()
+
+    for title in windowTitles:
+        if 'multimc' in title.lower():
+            print(title)
+            return title
+
+    pprint(windowTitles)
+    raise Exception("Could not find window title containing 'MultiMC'!")
+
+
+def focus_multimc() -> BaseWindow:
+
+    w: BaseWindow = gw.getWindowsWithTitle(get_multimc_window_title())[0]
+    w.activate()
+    return w
+
+
 def generate_modpack_zip():
+
+    print("Generating modpack zip at "+ZIP_NAME)
     if not os.path.exists("./pack.toml"):
-        raise Exception("Could not find pack.toml in current working directory '{}'!".format(os.getcwd()))
+        raise Exception(
+            "Could not find pack.toml in current working directory '{}'!".format(os.getcwd()))
 
     remove_file(ZIP_NAME)
     remove_file(DEFAULT_ZIP_NAME)
 
-    subprocess.check_output(['packwiz','cf','export'])
+    subprocess.check_output(['packwiz', 'cf', 'export'])
+    print("Done!")
+
     os.rename(DEFAULT_ZIP_NAME, ZIP_NAME)
 
-    
 
 if __name__ == '__main__':
     ensure_packwiz_installed()
     ensure_multimc_installed()
 
-    # ensure_multimc_closed()
-    # mmc_proc=open_multimc()
+    ensure_multimc_closed()
+    generate_modpack_zip()
+
+    mmc_proc = open_multimc()
+    time.sleep(10)
+
+    mmc_window = focus_multimc()
 
     '''
     <ESC>
     Add instance
     Import from zip
-    run `packwiz cf export`
     paste path
     click on ok
     wait for import
@@ -112,11 +155,8 @@ if __name__ == '__main__':
     exit 0 or 1!
     '''
 
-    print("potato")
     # time.sleep(10)
     # mmc_proc.kill() # kill MMC after 10s for testing
-    # print("killed mmc.")
-
-    generate_modpack_zip()
+    print("killed mmc.")
 
     exit(1)
