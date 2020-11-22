5c91aa214cfd922c7c38087f8ddd72dd64ad8032
Henry Post <HenryFBP@gmail.com>
2020-11-22 16:52:10 -0600

    print zip name when make

--- Makefile.py
+++ Makefile.py
@@ -2,15 +2,23 @@ import os
 import shutil
 import subprocess
 
+
 def get_git_revision_hash():
     return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode().strip()
+
 
 def get_git_revision_short_hash():
     return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode().strip()
 
+
 def get_latest_git_tag():
-	return subprocess.check_output(['git','describe','--tags','--abbrev=0']).decode().strip()
+    return subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode().strip()
 
-os.system("packwiz cf export")
 
-shutil.move('export.zip', f'rivers-of-iron-latest-{get_latest_git_tag()}-{get_git_revision_short_hash()}.zip')
+subprocess.check_output(["packwiz", "cf", "export"])
+
+outzip = f'rivers-of-iron-latest-{get_latest_git_tag()}-{get_git_revision_short_hash()}.zip'
+
+shutil.move('export.zip', outzip)
+
+print(f"Enjoy your pack at {outzip}")
